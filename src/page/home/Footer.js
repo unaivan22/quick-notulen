bca831b0840c40c9d9e3b6ff19d817f96becb5c9
unaivan <unaivan@gmail.com>
2023-02-24 09:31:22 +0700

    change editor

--- src/page/home/Footer.js
+++ src/page/home/Footer.js
@@ -4,13 +4,13 @@ export default function Footer() {
   return (
     <div className='grid grid-cols-2 pt-24'>
       <div className='grid grid-cols-1 text-center'>
-        <p className='mb-16 font-bold text-zinc-900'>PIC</p>
+        <p className='mb-24 font-bold text-zinc-900' contentEditable>PIC</p>
 
         <p contentEditable>Nama</p>
         <p className='text-sm text-zinc-700 font-light' contentEditable>Jabatan</p>
       </div>
       <div className='grid grid-cols-1 text-center'>
-        <p className='mb-16 font-bold text-zinc-900'>Client</p>
+        <p className='mb-24 font-bold text-zinc-900' contentEditable>Client</p>
 
         <p contentEditable>Nama</p>
         <p className='text-sm text-zinc-700 font-light' contentEditable>Jabatan</p>
--- src/page/home/HeadOne.js
+++ src/page/home/HeadOne.js
@@ -3,9 +3,10 @@ import React from 'react'
 export default function HeadOne() {
   return (
     <div className='grid grid-cols-1 border-b pb-4 border-zinc-300'>
-      <p className='col-span-6 w-full text-3xl text-rose-500 font-bold' contentEditable>Judul Meeting</p>
-      <p className='col-span-6 w-full text-zinc-500 text-base' contentEditable>Jl. Barata Jaya III No.16, Baratajaya, Kec. Gubeng, Kota SBY, Jawa Timur 60284</p>
-      <p className='col-span-6 w-full text-zinc-500 text-base' contentEditable>{(new Date().toLocaleDateString())}</p>
+      <p className='col-span-6 w-full text-3xl text-rose-500 font-bold' id='StepTitle' contentEditable>Judul Meeting</p>
+      <p className='col-span-6 w-full text-zinc-400 text-base'><span contentEditable>Nama Dinas</span>  |  <span contentEditable>nama aplikasi</span></p>
+      <p className='col-span-6 w-full text-zinc-400 text-base' contentEditable>Jl. Barata Jaya III No.16, Baratajaya, Kec. Gubeng, Kota SBY, Jawa Timur 60284</p>
+      <p className='col-span-6 w-full text-zinc-400 text-base' contentEditable>{(new Date().toLocaleDateString())}</p>
       {/* <div className='grid grid-cols-8'>
         <p className='col-span-2'>Judul Meeting</p>
         <p className='col-span-6 w-full'> <span className='mr-4'>:</span> <span contentEditable className='w-full'>isi</span></p>
