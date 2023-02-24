0b5d58599bf57b75ac54b393f1e3e5645655d15f
unaivan <unaivan@gmail.com>
2023-02-24 09:04:21 +0700

    change editor

--- src/page/home/Isi.js
+++ src/page/home/Isi.js
@@ -1,18 +1,31 @@
-import React from 'react'
-import { Editor } from "react-draft-wysiwyg";
-// import '../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
-import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+
+const  modules  = {
+  toolbar: [
+      [{ font: [] }],
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+      ["bold", "italic", "underline", "strike"],
+      [{ color: [] }, { background: [] }],
+      [{ script:  "sub" }, { script:  "super" }],
+      ["blockquote", "code-block"],
+      [{ list:  "ordered" }, { list:  "bullet" }],
+      [{ indent:  "-1" }, { indent:  "+1" }, { align: [] }],
+      ["link", "image", "video"],
+      ["clean"],
+  ],
+};
+
+
 
 export default function Isi() {
   return (
     <div  className='mt-12 mb-12'>
-      <Editor
-        // editorState={editorState}
-        // toolbarClassName="toolbarClassName"
-        // wrapperClassName="wrapperClassName"
-        // editorClassName="editorClassName"
-        // onEditorStateChange={this.onEditorStateChange}
-        />
+
+    <ReactQuill  modules={modules} theme="snow" placeholder="Masukkan notulen"  />
+    
     </div>
   )
 }
